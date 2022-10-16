be44fc94490de2a3b873d6276fad077ce5f5fc73
julian sepulveda <juliancamilo13@hotmail.com>
2022-10-16 18:04:08 -0300

    build(App): initial config for the proyect with webpack, babel and react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./style.module.css";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className={styles.app}>Hola</h1>
+      <h2>jucasero</h2>
+    </div>
+  );
+};
